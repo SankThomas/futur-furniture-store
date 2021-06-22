c8804b18ee6c398b971e603ebf960fd49bded48d
Thomas Sankara <thomassankara74@gmail.com>
2021-06-22 16:16:58 +0300

    render components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from "./components/Footer"
+import Grid from "./components/Grid"
+import Header from "./components/Header"
+import Newsletter from "./components/Newsletter"
+import Showcase from "./components/Showcase"
+import Testimonials from "./components/Testimonials"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header />
+      <Showcase />
+      <Grid />
+      <Testimonials />
+      <Newsletter />
+      <Footer />
+    </>
+  )
 }
 
-export default App;
+export default App
